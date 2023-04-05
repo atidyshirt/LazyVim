af0282516463da7698a444e5d39bdb7f601c6815
jordanp <jordanpyott@gmail.com>
2023-04-05 20:37:48 +1200

    MAINT: Adding keybinds for lsp workspace/document diagnostics

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -174,6 +174,10 @@ return {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
     opts = { use_diagnostic_signs = true },
+    keys = {
+      { "<leader>ld", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+      { "<leader>lw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+    },
   },
 
   -- todo comments
