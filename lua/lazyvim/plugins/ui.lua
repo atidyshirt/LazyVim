5bba32797599674eeec6e5f71c2bcc5decf0d764
jordanp <jordanpyott@gmail.com>
2023-04-13 22:46:09 +1200

    Revert "MAINT: display navic as a buffer context header"
    This reverts commit d38d86abfb5991f27289c1b2f823903c001936c1.

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -158,7 +158,7 @@ return {
     "SmiteshP/nvim-navic",
     lazy = true,
     init = function()
-      vim.g.navic_silence = false
+      vim.g.navic_silence = true
       require("lazyvim.util").on_attach(function(client, buffer)
         if client.server_capabilities.documentSymbolProvider then
           require("nvim-navic").attach(client, buffer)
