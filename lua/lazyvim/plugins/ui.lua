d38d86abfb5991f27289c1b2f823903c001936c1
jordanp <jordanpyott@gmail.com>
2023-04-13 22:42:41 +1200

    MAINT: display navic as a buffer context header

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -158,7 +158,7 @@ return {
     "SmiteshP/nvim-navic",
     lazy = true,
     init = function()
-      vim.g.navic_silence = true
+      vim.g.navic_silence = false
       require("lazyvim.util").on_attach(function(client, buffer)
         if client.server_capabilities.documentSymbolProvider then
           require("nvim-navic").attach(client, buffer)
