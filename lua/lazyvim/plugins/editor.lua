302c5b10a23597d6e651bbc26e9874b3c2dc90d1
jordanp <jordanpyott@gmail.com>
2023-05-07 09:29:41 +1200

    BUG: Changing passed in rip-grep options
    This is a patch to ensure that we do not use all our systems memory when
    using `Telescope live-grep` in large projects (or projects with binaries
    that are being grepped that probably shouldn't be).

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -80,6 +80,15 @@ return {
     },
     opts = {
       defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
         prompt_prefix = " ",
         selection_caret = " ",
         mappings = {
